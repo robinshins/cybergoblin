1cd7b5d7075cad4b8ee38893f8f67652b7ae17f8
robinshins <robinshins674@gmail.com>
2024-12-11 15:16:24 +0900

    유튜브 링크 추가

--- pages/chats/yoon.tsx
+++ pages/chats/yoon.tsx
@@ -281,6 +281,16 @@ export default function Yoon() {
             석열봇은 대통령실 홈페이지의 '대통령의 말과 글' <br/>
             내용들을 학습했습니다.
           </p>
+          <a 
+            href="https://www.youtube.com/watch?v=LFYf8peVPLs&t"
+            className="learn-more-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            석열봇 자세히 알아보기 <svg className="youtube-icon-small" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
+            </svg>
+          </a>
         </div>
 
         <div className="chat-area">
